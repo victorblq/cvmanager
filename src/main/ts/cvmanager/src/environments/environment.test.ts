d2aea7396f215053b5c9f85b26e108b75f24bfad
Victor Carvalho <victor.blq@gmail.com>
2020-03-25 21:44:13 -0300

    fixed url

--- src/main/ts/cvmanager/src/environments/environment.test.ts
+++ src/main/ts/cvmanager/src/environments/environment.test.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  base_url: "https://testecvmanager.azurewebsites.net/"
+  base_url: "https://testecvmanager.azurewebsites.net/cvmanager-1.0.0-SNAPSHOT/"
 };
